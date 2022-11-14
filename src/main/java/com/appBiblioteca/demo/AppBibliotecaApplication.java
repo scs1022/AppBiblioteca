0a37a00fc8fc39334bc25949454ee8aadb5bda6b
scs1022 <santiago22santisteban@gmail.com>
2022-11-14 13:02:04 -0500

    first commit

---
+++ src/main/java/com/appBiblioteca/demo/AppBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.appBiblioteca.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appBiblioteca/demo/AppBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appBiblioteca.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
